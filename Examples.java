7660c167cf9d3c85005713508a09a05edfed6a6a
Samhamsam <mrk.unger@gmail.com>
2016-04-25 02:01:19 +0200

    Added files

---
+++ Examples.java
@@ -0,0 +1,33 @@
+package testCases;
+import static org.junit.Assert.*;
+
+class Examples {
+	Examples () {}
+	
+	//initialize and make samples here to test run
+	
+	public void run(){
+		testnewgame();
+	}
+	
+	
+	//Test that a new game is generated
+	private boolean testnewgame () {
+		return true;
+	}
+	
+	//Test that the status bar displays the correct time
+	
+	
+	//Test that the status bar displays the correct number of bombs left
+	
+	//Test that Right Clicking will create a mark on a Tile
+	
+	//Test that Left Clicking a Tile will reveal what is underneath
+	
+	// Test that clicking a bomb ends the game
+	
+	//Test that clicking an empty tile continues the game
+	
+	
+}
---
+++ Mines.java
@@ -0,0 +1,18 @@
+package testCases;
+import java.awt.BorderLayout;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.SwingUtilities;
+
+
+public class Mines extends JFrame {
+	
+	private final int FrameWidth = 250;
+	private final int FrameHeight = 250;
+	//private final JLabel statusbar;
+	
+	public void Mines() {
+		setTitle("Minesweeper");
+		
+	}
+}
