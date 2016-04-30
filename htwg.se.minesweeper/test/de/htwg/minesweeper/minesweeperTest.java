c1ff853ef3562a2aaeec94afcdd01a4daeb217b5
Mark Unger <mrk.unger@gmail.com>
2016-04-30 22:11:31 +0200

    added first test

--- htwg.se.minesweeper/test/de/htwg/minesweeper/minesweeperTest.java
+++ htwg.se.minesweeper/test/de/htwg/minesweeper/minesweeperTest.java
@@ -1,1 +1,18 @@
+package de.htwg.minesweeper;
 
+
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+import de.htwg.minesweeper.*;
+
+public class minesweeperTest {
+	Field testField = new Field();
+	
+	@Test
+	public void setupFieldTest(){
+		
+		String test[][] = testField.setupField();
+		assertEquals(testField.column * testField.row, test.length * test[0].length);
+	}
+}
