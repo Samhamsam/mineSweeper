2fda0850ef96e82973c0f79ca7af8324b0d35785
Mark Unger <mrk.unger@gmail.com>
2016-04-30 12:18:04 +0200

    changed folders

---
+++ htwg.se.minesweeper/src/de/htwg/minesweeper/controller/IObservable.java
@@ -0,0 +1,13 @@
+package de.htwg.util.observer
+
+public class IObservable {
+	void addObserver(IObserver s);
+
+    void removeObserver(IObserver s);
+
+    void removeAllObservers();
+
+    void notifyObservers();
+
+    void notifyObservers(Event e);
+}
