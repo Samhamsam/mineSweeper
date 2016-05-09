8e29cc5659c6c974b39846d8a3837ba4bc0e697e
Leah Holt <lholt@wpi.edu>
2016-05-09 20:41:17 +0200

    Add files via upload

---
+++ htwg.se.minesweeper/src/de/htwg/minesweeper/BlockPanel.java
@@ -0,0 +1,14 @@
+package de.htwg.minesweeper.aview.gui;
+
+import java.awt.GridLayout;
+
+import javax.swing.JPanel;
+
+public class BlockPanel extends JPanel {
+
+    private static final long serialVersionUID = 9094365991959087973L;
+
+    public BlockPanel(int blockSize) {
+        setLayout(new GridLayout(blockSize, blockSize, 1, 1));
+    }
+}
