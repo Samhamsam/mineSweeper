0468b92febb6e17e2d65f77544efbf84aab8942c
Leah Holt <lholt@wpi.edu>
2016-06-07 14:54:45 +0200

    Update Main.java

--- src/de/htwg/minesweeper/Main.java
+++ src/de/htwg/minesweeper/Main.java
@@ -2,11 +2,7 @@ package de.htwg.minesweeper;
 
 import de.htwg.minesweeper.controller.MineSweeperController;
 
-
-//import tester.*;
-
 public class Main {
-	//static Examples E = new Examples ();
 	
 	public static void main (String args[]) {
 		
