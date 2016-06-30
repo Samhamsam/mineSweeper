9709140a23fd4b635141485a821caf015679bec6
Leah Holt <lholt@wpi.edu>
2016-06-30 13:22:45 +0200

    Update Help.java

--- src/de/htwg/minesweeper/controller/Help.java
+++ src/de/htwg/minesweeper/controller/Help.java
@@ -6,8 +6,8 @@ public class Help {
 	
 	public void run()
 	{
-		String text = "(TUI:n) GUI:	Menu	->	New Game: 	This command start a new game. (reset)\n"
-					+ "(TUI:q) GUI:	Menu	->	Quit:		This command end the Game and close it\n"
+		String text = "(TUI:n) GUI:	Menu	->	New Game: 	This command starts a new game. (reset)\n"
+					+ "(TUI:q) GUI:	Menu	->	Quit:		This command ends the Game and close it\n"
 					+ "(TUI:h) GUI:	?	->	Help:		This command shows the help text";
 		setHelpText(text);
 	}
